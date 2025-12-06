466d63bdee37ba1f75b5e87808be3e8becea6693
tasin <tasin17@hotmail.com>
2025-12-06 11:25:29 +0000

    added time for python script

--- python/gun_detect.py
+++ python/gun_detect.py
@@ -1,5 +1,6 @@
 import cv2
 import torch
+import time
 
 def preprocess_image(image_ori) -> torch.Tensor:
     image = cv2.resize(image_ori, (640, 640))
@@ -10,7 +11,10 @@ def preprocess_image(image_ori) -> torch.Tensor:
     image_tensor = image_tensor.unsqueeze(0)
     return image_tensor, image_rgb
 
-torchmodel = torch.jit.load("../best.torchscript", map_location='cuda')
+device = 'cuda'
+#device = 'cpu'
+
+torchmodel = torch.jit.load("../best.torchscript", map_location=device)
 torchmodel.eval()
 cap = cv2.VideoCapture('../evaluation.mp4')
 
@@ -19,8 +23,10 @@ with torch.no_grad():
         ret, frame = cap.read()
         if ret == True:
             image, image_rgb = preprocess_image(frame)
-            image = image.cuda()
+            image = image.to(device)
+            start_time = time.time() 
             results = torchmodel(image)
+            end_time = time.time()
             result = results[0]  # Remove the batch dimension if there's only one image
             boxes = result[:4, :]
             person_prob = torch.argmax(result[4])
@@ -33,7 +39,14 @@ with torch.no_grad():
                 x2 = int(x1 + w)
                 y2 = int(y1 + h)
                 cv2.rectangle(image_rgb, (x1, y1), (x2, y2), (0, 0, 255), 2)
-
+            duration_ms = (end_time - start_time) * 1000
+            text = f"Inference: {duration_ms:.2f} ms"
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            font_scale = 0.7
+            thickness = 2
+            org = (10, 30) # top-left corner
+            color = (0, 255, 0) # green (BGR)
+            cv2.putText(image_rgb, text, org, font, font_scale, color, thickness)
             cv2.imshow('Frame', image_rgb)
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
