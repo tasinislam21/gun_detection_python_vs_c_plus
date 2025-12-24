363f82adf317aec5135dbbb9d5b04648acbad0d2
tasin <tasin17@hotmail.com>
2025-12-24 11:47:01 +0000

    refactored c++

--- c++/detect_firearm.cpp
+++ c++/detect_firearm.cpp
@@ -11,6 +11,8 @@ struct PreprocessResult {
     cv::Mat rgb_image;
 };
 
+torch::jit::script::Module model;
+
 PreprocessResult preprocess_image(const cv::Mat& image_ori) {
     PreprocessResult out;
     cv::Mat resized, image_rgb;
@@ -31,11 +33,56 @@ PreprocessResult preprocess_image(const cv::Mat& image_ori) {
     return out;
 }
 
+void load_model(torch::Device device) {
+    // Deserialize the ScriptModule from a file using torch::jit::load().
+    model = torch::jit::load("../../best.torchscript", device=device);
+    std::cout << "Model loaded successfully\n";
+}
+
+torch::Tensor get_boxes(const torch::Tensor& result) {
+    // The first 4 elements are bounding box coordinates
+    return result.slice(0, 0, 4); // column 0..3
+}
+
+int get_highest_firearm_prob(const torch::Tensor& result) {
+    return std::get<1>(result[5].max(0)).to(torch::kCPU).item<int>();
+}
+
+void draw_bounding_box(PreprocessResult& preprocessed_images, torch::Tensor& box, int& highest_firearm_index) {
+    // Drawing bounding boxes on the frame 
+    torch::Tensor box_tensor = box.index({torch::indexing::Slice(), highest_firearm_index});
+    box_tensor = box_tensor.cpu();
+
+    auto box_np = box_tensor.contiguous().data_ptr<float>();
+
+    float x = box_np[0];
+    float y = box_np[1];
+    float w = box_np[2];
+    float h = box_np[3];
+
+    int x1 = int(x - w / 2);
+    int y1 = int(y - h / 2);
+    int x2 = int(x1 + w);
+    int y2 = int(y1 + h);
+
+    cv::rectangle(preprocessed_images.rgb_image, cv::Point(x1, y1), cv::Point(x2, y2),
+                    cv::Scalar(0, 0, 255), 2);
+}
+
+void include_inference_time(std::chrono::milliseconds& duration, PreprocessResult& preprocessed_images) {
+    std::string text = "Inference: " + std::to_string(duration.count()) + " ms"; 
+    int font = cv::FONT_HERSHEY_SIMPLEX; 
+    double fontScale = 0.7; int thickness = 2; 
+    cv::Point org(10, 30); // top-left corner 
+    cv::Scalar color(0, 255, 0); // green 
+    cv::putText(preprocessed_images.rgb_image, text, org, font, fontScale, color, thickness);
+
+}
+
 int main() {
     torch::Device device(torch::kCUDA);
-    //torch::Device device(torch::kCPU);
-    torch::jit::script::Module model = torch::jit::load("../../best.torchscript", device=device);   
-    std::cout << "Created model success!" << std::endl;
+    load_model(device);
+    
     cv::VideoCapture cap("../../evaluation.mp4");
     cv::Mat frame;
     
@@ -45,42 +92,17 @@ int main() {
         torch::Tensor input = preprocessed_images.tensor;
         input = input.to(device);
         auto start_time = std::chrono::high_resolution_clock::now();
-        auto results = model.forward({input}).toTensor();
+        torch::Tensor result = model.forward({input}).toTensor()[0]; //extract the first element from the batch
         auto end_time = std::chrono::high_resolution_clock::now();
-        auto result = results[0];
-        
-        auto boxes = result.slice(0, 0, 4);  // column 0..3
-        auto person_prob = std::get<1>(result[4].max(0));
-        auto gun_prob = std::get<1>(result[5].max(0));
-
-        float gun_conf = result[5][gun_prob].item<float>();
+        torch::Tensor boxes = get_boxes(result); 
+        int highest_firearm_index = get_highest_firearm_prob(result); 
+        float gun_conf = result[5][highest_firearm_index].item<float>();
 
         if (gun_conf > 0.35) {
-                torch::Tensor box_tensor = boxes.index({torch::indexing::Slice(), gun_prob});
-                box_tensor = box_tensor.cpu();
-
-                auto box_np = box_tensor.contiguous().data_ptr<float>();
-
-                float x = box_np[0];
-                float y = box_np[1];
-                float w = box_np[2];
-                float h = box_np[3];
-
-                int x1 = int(x - w / 2);
-                int y1 = int(y - h / 2);
-                int x2 = int(x1 + w);
-                int y2 = int(y1 + h);
-
-                cv::rectangle(preprocessed_images.rgb_image, cv::Point(x1, y1), cv::Point(x2, y2),
-                              cv::Scalar(0, 0, 255), 2);
+                draw_bounding_box(preprocessed_images, boxes, highest_firearm_index);
             }
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
-        std::string text = "Inference: " + std::to_string(duration.count()) + " ms"; 
-        int font = cv::FONT_HERSHEY_SIMPLEX; 
-        double fontScale = 0.7; int thickness = 2; 
-        cv::Point org(10, 30); // top-left corner 
-        cv::Scalar color(0, 255, 0); // green 
-        cv::putText(preprocessed_images.rgb_image, text, org, font, fontScale, color, thickness);
+        std::chrono::milliseconds duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+        include_inference_time(duration, preprocessed_images);
         cv::imshow("Frame", preprocessed_images.rgb_image);
             if ((cv::waitKey(25) & 0xFF) == 'q'){
                 break;
