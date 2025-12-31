b4e6a47dfd3cb09fc0c7b011b3cd810bb23d54e1
tasin <tasin17@hotmail.com>
2025-12-31 14:09:33 +0000

    c++ performs NMS for multi weapon detection

--- c++/detect_firearm.cpp
+++ c++/detect_firearm.cpp
@@ -5,16 +5,24 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <vector>
 
-struct PreprocessResult {
+std::vector<int> non_max_suppression(const std::vector<torch::Tensor>& boxes, const std::vector<torch::Tensor>& scores, float iou_threshold);
+
+struct PreprocessImage {
     torch::Tensor tensor;
     cv::Mat rgb_image;
 };
 
+struct FilteredResult {
+    std::vector<torch::Tensor> boxes;
+    std::vector<torch::Tensor> scores;
+};
+    
 torch::jit::script::Module model;
 
-PreprocessResult preprocess_image(const cv::Mat& image_ori) {
-    PreprocessResult out;
+PreprocessImage preprocess_image(const cv::Mat& image_ori) {
+    PreprocessImage out;
     cv::Mat resized, image_rgb;
     cv::resize(image_ori, resized, cv::Size(640, 640));
     cv::cvtColor(resized, image_rgb, cv::COLOR_BGR2RGB);
@@ -44,32 +52,47 @@ torch::Tensor get_boxes(const torch::Tensor& result) {
     return result.slice(0, 0, 4); // column 0..3
 }
 
-int get_highest_firearm_prob(const torch::Tensor& result) {
-    return std::get<1>(result[5].max(0)).to(torch::kCPU).item<int>();
+FilteredResult filter_result(const torch::Tensor& result) {
+    FilteredResult out;
+    torch::Tensor boxes = result.index({torch::indexing::Slice(0,4), torch::indexing::Slice()});
+    torch::Tensor scores = result.index({5});
+    torch::Tensor mask = scores > 0.35;
+    if (mask.any().item<bool>()) {
+        torch::Tensor boxes_filtered = boxes.index({torch::indexing::Slice(), mask});
+        torch::Tensor scores_filtered = scores.index({mask});
+
+        int K = boxes_filtered.size(1); // # of boxes after filtering
+
+        // Convert to vector<torch::Tensor>
+        for (int i = 0; i < K; ++i) {
+            out.boxes.push_back(boxes_filtered.index({torch::indexing::Slice(), i}));
+            out.scores.push_back(scores_filtered.index({i}));
+        }
+    } 
+    return out;
 }
 
-void draw_bounding_box(PreprocessResult& preprocessed_images, torch::Tensor& box, int& highest_firearm_index) {
+void draw_bounding_box(PreprocessImage& preprocessed_images, FilteredResult& filteredResult, std::vector<int>& indexes) {
     // Drawing bounding boxes on the frame 
-    torch::Tensor box_tensor = box.index({torch::indexing::Slice(), highest_firearm_index});
-    box_tensor = box_tensor.cpu();
-
-    auto box_np = box_tensor.contiguous().data_ptr<float>();
-
-    float x = box_np[0];
-    float y = box_np[1];
-    float w = box_np[2];
-    float h = box_np[3];
-
-    int x1 = int(x - w / 2);
-    int y1 = int(y - h / 2);
-    int x2 = int(x1 + w);
-    int y2 = int(y1 + h);
-
-    cv::rectangle(preprocessed_images.rgb_image, cv::Point(x1, y1), cv::Point(x2, y2),
-                    cv::Scalar(0, 0, 255), 2);
+    for (int index: indexes) {
+        torch::Tensor box_tensor = filteredResult.boxes[index];
+        box_tensor = box_tensor.cpu();
+        auto box_np = box_tensor.contiguous().data_ptr<float>();
+        float x = box_np[0];
+        float y = box_np[1];
+        float w = box_np[2];
+        float h = box_np[3];
+        int x1 = int(x - w / 2);
+        int y1 = int(y - h / 2);
+        int x2 = int(x1 + w);
+        int y2 = int(y1 + h);
+        cv::rectangle(preprocessed_images.rgb_image, cv::Point(x1, y1), cv::Point(x2, y2),
+                        cv::Scalar(0, 0, 255), 2);
+    }
+  
 }
 
-void include_inference_time(std::chrono::milliseconds& duration, PreprocessResult& preprocessed_images) {
+void include_inference_time(std::chrono::milliseconds& duration, PreprocessImage& preprocessed_images) {
     std::string text = "Inference: " + std::to_string(duration.count()) + " ms"; 
     int font = cv::FONT_HERSHEY_SIMPLEX; 
     double fontScale = 0.7; int thickness = 2; 
@@ -88,19 +111,19 @@ int main() {
     
     torch::NoGradGuard no_grad;
     while (cap.read(frame)) {
-        PreprocessResult preprocessed_images = preprocess_image(frame);
+        PreprocessImage preprocessed_images = preprocess_image(frame);
         torch::Tensor input = preprocessed_images.tensor;
         input = input.to(device);
         auto start_time = std::chrono::high_resolution_clock::now();
         torch::Tensor result = model.forward({input}).toTensor()[0]; //extract the first element from the batch
         auto end_time = std::chrono::high_resolution_clock::now();
         torch::Tensor boxes = get_boxes(result); 
-        int highest_firearm_index = get_highest_firearm_prob(result); 
-        float gun_conf = result[5][highest_firearm_index].item<float>();
-
-        if (gun_conf > 0.35) {
-                draw_bounding_box(preprocessed_images, boxes, highest_firearm_index);
-            }
+        FilteredResult filteredResult = filter_result(result);  // we only want confidence scores greater than 35%
+        std::vector<int> supressed_indexes;
+        if (filteredResult.boxes.size() > 0) {
+            supressed_indexes = non_max_suppression(filteredResult.boxes, filteredResult.scores, 0.5);
+        }
+        draw_bounding_box(preprocessed_images, filteredResult, supressed_indexes);
         std::chrono::milliseconds duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
         include_inference_time(duration, preprocessed_images);
         cv::imshow("Frame", preprocessed_images.rgb_image);
@@ -109,4 +132,4 @@ int main() {
                 }
         }
     return 0;
-    }
+}
---
+++ c++/nms.cpp
@@ -0,0 +1,66 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <torch/torch.h>
+#include <torch/script.h>
+
+float iou(torch::Tensor box1, torch::Tensor box2) {
+    float x1_min = (box1[0].item<float>() - box1[2].item<float>())  / 2;
+    float y1_min = (box1[1].item<float>() - box1[3].item<float>()) / 2;
+    float x1_max = (box1[0].item<float>() + box1[2].item<float>()) / 2;
+    float y1_max = (box1[1].item<float>() + box1[3].item<float>()) / 2;
+
+    float x2_min = (box2[0].item<float>() - box2[2].item<float>()) / 2;
+    float y2_min = (box2[1].item<float>() - box2[3].item<float>()) / 2;
+    float x2_max = (box2[0].item<float>() + box2[2].item<float>()) / 2;
+    float y2_max = (box2[1].item<float>() + box2[3].item<float>()) / 2;
+
+    float inter_x_min = std::max(x1_min, x2_min);
+    float inter_y_min = std::max(y1_min, y2_min);
+    float inter_x_max = std::min(x1_max, x2_max);
+    float inter_y_max = std::min(y1_max, y2_max);
+
+    float inter_width = std::max(0.0f, inter_x_max - inter_x_min);
+    float inter_height = std::max(0.0f, inter_y_max - inter_y_min);
+    float inter_area = inter_width * inter_height;
+
+    float area1 = box1[2].item<float>() * box1[3].item<float>();
+    float area2 = box2[2].item<float>() * box2[3].item<float>();
+
+    float union_area = area1 + area2 - inter_area;
+    return inter_area / union_area;
+}
+
+std::vector<int> non_max_suppression(const std::vector<torch::Tensor>& boxes, const std::vector<torch::Tensor>& scores, float iou_threshold){
+    std::vector<int> indices(scores.size());
+    std::vector<int> keep;
+
+    for (size_t i = 0; i < scores.size(); i++) {
+        indices[i] = i;
+    }
+
+    std::vector<float> score_values(scores.size());
+    for (size_t i = 0; i < scores.size(); i++) {
+        score_values[i] = scores[i].item<float>();
+    }
+
+    std::sort(indices.begin(), indices.end(),
+            [&](int a, int b) {
+                return score_values[a] > score_values[b];
+            });
+
+    while (!indices.empty()) {
+        int current = indices.front();
+        indices.erase(indices.begin()); 
+        keep.push_back(current);
+
+        std::vector<int> remaining;
+        for (int i : indices) {
+            if (iou(boxes[current], boxes[i]) < iou_threshold) {
+                remaining.push_back(i);
+            }   
+        }
+        indices = std::move(remaining);
+    }
+    return keep;
+}
